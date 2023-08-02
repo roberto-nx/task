833db42d6007bb1f258e50fad2bdb44f85f7b414
Roberto Chagas <roberto.ca.dev@gmail.com>
2023-08-02 14:25:52 -0300

    Setup inertia Client-side

---
+++ resources/js/Pages/task.vue
@@ -0,0 +1,17 @@
+
+<template>
+  <Layout>
+    <Head title="Welcome" />
+    <h1>Welcome</h1>
+    <p>Hello {{ user.name }}, welcome to your first Inertia app!</p>
+  </Layout>
+</template>
+<style scoped>
+</style>
+
+<script setup>
+import Layout from './Layout'
+import { Head } from '@inertiajs/vue3'
+
+defineProps({ user: Object })
+</script>
